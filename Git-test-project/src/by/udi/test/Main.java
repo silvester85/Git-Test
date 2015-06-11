d6758734a933e8e843c20e5bca8fb80d04f2ad0b
silvester85 <nod3847@yandex.ru>
2015-06-12 00:46:10 +0300

    Add one !

--- Git-test-project/src/by/udi/test/Main.java
+++ Git-test-project/src/by/udi/test/Main.java
@@ -3,7 +3,7 @@ package by.udi.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git Hub !!!");
+		System.out.println("Hello Git Hub !!!!");
 
 	}
 
